566afd72ccf17255840c699015f7399775f7d1d4
Jeo Tecson <jeomayku.tecson@lsu.edu.ph>
2025-10-27 23:42:38 +0800

    Added admin

--- lib/admin.ts
+++ lib/admin.ts
@@ -3,6 +3,7 @@ import { auth } from "@clerk/nextjs/server";
 const adminIds = [
   "user_2sc5YzPPZ6uXl42r4rzmvcsIgfG",
   "user_34MxwSnCyiUn8kRbSi9GjJomdB2",
+  "user_34MyNNIi75mafzTCTNStg9UfHdm",
 ];
 
 export const getIsAdmin = async () => {
