22ade06c0dd1aaac1491bbcfa5de9eda624decdb
Jeo Tecson <jeomayku.tecson@lsu.edu.ph>
2025-10-29 20:14:29 +0800

    Update admin check logic with Clerk integration

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,17 +1,10 @@
-import { auth } from "@clerk/nextjs/server";
-
-const adminIds = [
-  "user_2sc5YzPPZ6uXl42r4rzmvcsIgfG",
-  "user_34MxwSnCyiUn8kRbSi9GjJomdB2",
-  "user_34MyNNIi75mafzTCTNStg9UfHdm",
-];
+import { auth, clerkClient } from "@clerk/nextjs/server";
 
 export const getIsAdmin = async () => {
   const { userId } = await auth();
 
-  if (!userId) {
-    return false;
-  }
+  if (!userId) return false;
 
-  return adminIds.indexOf(userId) !== -1;
+  const user = await clerkClient.users.getUser(userId);
+  return user.privateMetadata.role === "admin";
 };
