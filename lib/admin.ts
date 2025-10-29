190a6d9ccd5d558443e219ec44c8e0193474505a
Jeo Tecson <jeomayku.tecson@lsu.edu.ph>
2025-10-29 20:27:33 +0800

    feat: Check admin status from Clerk metadata

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,10 +1,13 @@
-import { auth, clerkClient } from "@clerk/nextjs/server";
+// lib/admin.ts
+import { auth } from "@clerk/nextjs/server";
 
 export const getIsAdmin = async () => {
-  const { userId } = await auth();
+  const { sessionClaims } = await auth();
 
-  if (!userId) return false;
+  //If there's no session, the user is not logged in, so they can't be an admin.
+  if (!sessionClaims) {
+    return false;
+  }
 
-  const user = await clerkClient.users.getUser(userId);
-  return user.privateMetadata.role === "admin";
+  return sessionClaims?.publicMetadata?.isAdmin === true;
 };
